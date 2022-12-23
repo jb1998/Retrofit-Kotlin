06955d97774542ae6f5d7db72fe7812362d9787c
Jatin Bindra <jatinbindra171998@gmail.com>
2022-12-23 18:40:33 -0500

    Initial commit

---
+++ app/src/main/java/com/aarafrao/postreqdemo/ServiceBuilder.kt
@@ -0,0 +1,19 @@
+package com.aarafrao.postreqdemo
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://api.server.com") // this base url can be one where server is running
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
